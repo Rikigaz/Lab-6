58a711f96800af69dc8f389b71701dcb2c16ce7f
hz132 <muzyki99@bk.ru>
2022-10-08 09:31:54 +0300

    blood6.2

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,5 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.BlogListView.as_view()),
-    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail')
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
 ]
